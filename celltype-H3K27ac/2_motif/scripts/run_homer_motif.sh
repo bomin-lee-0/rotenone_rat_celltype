216a5d1140f64be4ca01bf008d5e83bffc30d754
이보민 <bomin@ibomin-ui-MacBookPro.local>
2025-12-02 15:12:21 +0000

    Add HOMER motif script and simplify LDSC README
    - Add run_homer_motif.sh for cell type-specific motif analysis
    - Simplify LDSC README from 800 lines to 51 lines

---
+++ celltype-H3K27ac/2_motif/scripts/run_homer_motif.sh
@@ -0,0 +1,19 @@
+# Neg (Microglia)
+/gpfs/data01/cbenner/software/homer/bin/findMotifsGenome.pl \
+/scratch/prj/bcn_marzi_lab/ratlas/Bomin/merged_final/Neg_clean.bed \
+rn7 /scratch/prj/bcn_marzi_lab/ratlas/Bomin/motif/Neg -size 200 -len 8,10,12 -S 10
+
+# NeuN (Neurons)
+/gpfs/data01/cbenner/software/homer/bin/findMotifsGenome.pl \
+/scratch/prj/bcn_marzi_lab/ratlas/Bomin/merged_final/NeuN_clean.bed \
+rn7 /scratch/prj/bcn_marzi_lab/ratlas/Bomin/motif/NeuN -size 200 -len 8,10,12 -S 10
+
+# Nurr (Dopaminergic)
+/gpfs/data01/cbenner/software/homer/bin/findMotifsGenome.pl \
+/scratch/prj/bcn_marzi_lab/ratlas/Bomin/merged_final/Nurr_clean.bed \
+rn7 /scratch/prj/bcn_marzi_lab/ratlas/Bomin/motif/Nurr -size 200 -len 8,10,12 -S 10
+
+# Olig (Oligodendrocyte)
+/gpfs/data01/cbenner/software/homer/bin/findMotifsGenome.pl \
+/scratch/prj/bcn_marzi_lab/ratlas/Bomin/merged_final/Olig_clean.bed \
+rn7 /scratch/prj/bcn_marzi_lab/ratlas/Bomin/motif/Olig -size 200 -len 8,10,12 -S 10
