70640ad07c806e39f62b7826e95b246219dcd7e3
이보민 <bomin@ibomin-ui-MacBookPro.local>
2025-12-02 16:12:28 +0000

    Update HOMER motif script with installation and usage instructions

--- celltype-H3K27ac/2_motif/scripts/run_homer_motif.sh
+++ celltype-H3K27ac/2_motif/scripts/run_homer_motif.sh
@@ -2,22 +2,41 @@
 # Note: The same command works for both intersect and non-intersect analysis.
 #       Just change the input BED file accordingly.
 
+# ============================================================
+# Installation (using conda)
+# ============================================================
+# conda create -n homer -c bioconda homer
+# conda activate homer
+# which findMotifsGenome.pl
+
+# ============================================================
+# Usage
+# ============================================================
+# findMotifsGenome.pl <input.bed> <genome> <output_dir> [options]
+#   -size 200      : Region size (±100bp around peak center)
+#   -len 8,10,12   : Motif lengths to search
+#   -S 10          : Number of motifs to find
+
+# ============================================================
+# Run motif analysis for each cell type
+# ============================================================
+
 # Neg (Microglia)
-/gpfs/data01/cbenner/software/homer/bin/findMotifsGenome.pl \
-/scratch/prj/bcn_marzi_lab/ratlas/Bomin/merged_final/Neg_clean.bed \
-rn7 /scratch/prj/bcn_marzi_lab/ratlas/Bomin/motif/Neg -size 200 -len 8,10,12 -S 10
+findMotifsGenome.pl \
+  ./merged_final/Neg_clean.bed \
+  rn7 ./motif/Neg -size 200 -len 8,10,12 -S 10
 
 # NeuN (Neurons)
-/gpfs/data01/cbenner/software/homer/bin/findMotifsGenome.pl \
-/scratch/prj/bcn_marzi_lab/ratlas/Bomin/merged_final/NeuN_clean.bed \
-rn7 /scratch/prj/bcn_marzi_lab/ratlas/Bomin/motif/NeuN -size 200 -len 8,10,12 -S 10
+findMotifsGenome.pl \
+  ./merged_final/NeuN_clean.bed \
+  rn7 ./motif/NeuN -size 200 -len 8,10,12 -S 10
 
 # Nurr (Dopaminergic)
-/gpfs/data01/cbenner/software/homer/bin/findMotifsGenome.pl \
-/scratch/prj/bcn_marzi_lab/ratlas/Bomin/merged_final/Nurr_clean.bed \
-rn7 /scratch/prj/bcn_marzi_lab/ratlas/Bomin/motif/Nurr -size 200 -len 8,10,12 -S 10
+findMotifsGenome.pl \
+  ./merged_final/Nurr_clean.bed \
+  rn7 ./motif/Nurr -size 200 -len 8,10,12 -S 10
 
 # Olig (Oligodendrocyte)
-/gpfs/data01/cbenner/software/homer/bin/findMotifsGenome.pl \
-/scratch/prj/bcn_marzi_lab/ratlas/Bomin/merged_final/Olig_clean.bed \
-rn7 /scratch/prj/bcn_marzi_lab/ratlas/Bomin/motif/Olig -size 200 -len 8,10,12 -S 10
+findMotifsGenome.pl \
+  ./merged_final/Olig_clean.bed \
+  rn7 ./motif/Olig -size 200 -len 8,10,12 -S 10
